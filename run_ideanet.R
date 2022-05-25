11a010aa22c96334f4da7399b4f5f78430c6b93b
Dan McLinden <65354585+ideanetwork@users.noreply.github.com>
2022-05-25 13:00:50 -0400

    add mirror & library

--- run_ideanet.R
+++ run_ideanet.R
@@ -1,4 +1,9 @@
+options(repos=structure(c(CRAN="http://cran.us.r-project.org")))   # select a cran mirror to install packages if needed
 
+if (!require(pacman)){
+  install.packages("pacman")
+  suppressPackageStartupMessages(library(pacman))
+}
 pacman::p_unlock() # remove 00lock if present
 
 Ideanet::ideanet() # run the application.
